7adcc18bfd9ec9ae714a964babfd9247308fcbb3
jaykun <junkaiong11@gmail.com>
2018-10-14 08:20:59 +0000

    Add Post class setters and getters

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -1,7 +1,45 @@
+import java.sql.*
+
 public class Post
 {
     private int postid
     private String username;
     private String title;
     private String body;
+    private TimeStamp modified;
+    private TimStamp created;
+
+    public Post(int id, String username, String title, String body)
+    {
+        super();
+        this.postid = id;
+        this.username = username;
+        this.title = title;
+        this.body = body;
+    }
+
+    public int getPostId()
+    {
+        return postId;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public String getBody()
+    {
+        return body;
+    }
+
+    public String getModified()
+    {
+        return modified.toString();
+    }
+
+    public String getCreated()
+    {
+        return created.toString();
+    }
 }
